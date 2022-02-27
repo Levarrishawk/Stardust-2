05e78cbb195db80e93822e7555bc1168770f4c9b
Levarrishawk <levarrishawk@gmail.com>
2022-02-27 02:14:50 -0500

    [merge] Armor Object Scripts

---
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/infiltrator/objects.lua
@@ -0,0 +1,160 @@
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_belt = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_belt.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_belt, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_belt.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bicep_l = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bicep_l.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bicep_l, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bicep_l.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bicep_r = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bicep_r.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bicep_r, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bicep_r.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_boots = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_boots.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_boots, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_boots.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bracer_l = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bracer_l.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bracer_l, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bracer_l.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bracer_r = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bracer_r.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_bracer_r, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bracer_r.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_chest_plate = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_chest_plate.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_chest_plate, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_chest_plate.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_gloves = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_gloves.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_gloves, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_gloves.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_helmet = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_helmet.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_helmet, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_helmet.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_leggings = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_leggings.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s01_leggings, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_leggings.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_belt = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_belt.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_belt, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_belt.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bicep_l = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bicep_l.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bicep_l, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bicep_l.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bicep_r = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bicep_r.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bicep_r, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bicep_r.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_boots = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_boots.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_boots, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_boots.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bracer_l = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bracer_l.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bracer_l, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bracer_l.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bracer_r = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bracer_r.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_bracer_r, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_bracer_r.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_chest_plate = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_chest_plate.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_chest_plate, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_chest_plate.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_gloves = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_gloves.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_gloves, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_gloves.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_helmet = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_helmet.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_helmet, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_helmet.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_leggings = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_leggings.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_infiltrator_shared_armor_infiltrator_s02_leggings, "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s02_leggings.iff")
+------------------------------------------------------------------------------------------------------------------------------------
+
+
---
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian_sabine/objects.lua
@@ -0,0 +1,74 @@
+
+
+
+--****************************************************************************************************************************************************************************
+object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bicep_l = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bicep_l.iff"
+
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bicep_l, "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bicep_l.iff")
+
+--****************************************************************************************************************************************************************************
+
+--****************************************************************************************************************************************************************************
+object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bicep_r = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bicep_r.iff"
+
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bicep_r, "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bicep_r.iff")
+
+--****************************************************************************************************************************************************************************
+
+--****************************************************************************************************************************************************************************
+object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_boots = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_boots.iff"
+
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_boots, "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_boots.iff")
+
+--****************************************************************************************************************************************************************************
+
+--****************************************************************************************************************************************************************************
+object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bracer_l = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bracer_l.iff"
+
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bracer_l, "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bracer_l.iff")
+
+--****************************************************************************************************************************************************************************
+
+--****************************************************************************************************************************************************************************
+object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bracer_r = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bracer_r.iff"
+
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_bracer_r, "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_bracer_r.iff")
+
+--****************************************************************************************************************************************************************************
+
+--****************************************************************************************************************************************************************************
+object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_chest_plate = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_chest_plate.iff"
+
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_chest_plate, "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_chest_plate.iff")
+
+--****************************************************************************************************************************************************************************
+
+
+--****************************************************************************************************************************************************************************
+object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_leggings= SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_leggings.iff"
+
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_mandalorian_sabine_shared_armor_mandalorian_sabine_leggings, "object/tangible/wearables/armor/mandalorian_sabine/shared_armor_mandalorian_sabine_leggings.iff")
+
+--****************************************************************************************************************************************************************************
+
