da8b0536df3938d53e7f9d6d242ca4028b31780f
Levarrishawk <levarrishawk@gmail.com>
2022-03-05 04:15:11 -0500

    [iteration] Kaas Screenplays

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -764,9 +764,9 @@ includeFile("poi/kaas_smuggler_starport.lua")
 includeFile("heroics/chiss_hunter_herald.lua")
 includeFile("heroics/chiss_hunter_herald_cave.lua")
 
---includeFile("poi/kaas_city_ruins_mellichae.lua")
---includeFile("poi/kaas_abandon_separ_facility_necrosis.lua")
---includeFile("poi/kaas_vitiate_tomb.lua")
+includeFile("poi/kaas_city_ruins_mellichae.lua")
+includeFile("poi/kaas_abandon_separ_facility_necrosis.lua")
+includeFile("poi/kaas_vitiate_tomb.lua")
 
 
 
