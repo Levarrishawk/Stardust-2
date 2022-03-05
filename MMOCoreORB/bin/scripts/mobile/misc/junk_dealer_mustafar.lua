0fb58314227ab35337ef3274e8c219b05eed21a6
Levarrishawk <levarrishawk@gmail.com>
2022-03-05 01:00:54 -0500

    [iteration] Kaas

---
+++ MMOCoreORB/bin/scripts/mobile/misc/junk_dealer_mustafar.lua
@@ -0,0 +1,37 @@
+junk_dealer_mustafar = Creature:new {
+	objectName = "@mob/creature_names:junk_dealer",
+	customName = "Mensix Corp. Merchant",
+	planetMapCategory = "junkshop",
+	socialGroup = "townsperson",
+	faction = "townsperson",
+	level = 100,
+	chanceHit = 1,
+	damageMin = 645,
+	damageMax = 1000,
+	baseXp = 9429,
+	baseHAM = 24000,
+	baseHAMmax = 30000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 2 * 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = INVULNERABLE + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/som/mustafarian_m_01.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "junkDealerGenericConvoTemplate",
+	attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(junk_dealer_mustafar, "junk_dealer_mustafar")
