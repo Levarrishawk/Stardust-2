bada3a842105a1fe1ef261d952ab9e7b106d3d8d
Levarrishawk <levarrishawk@gmail.com>
2022-04-25 01:43:01 -0400

    [iteration] Activated EK Screenplays

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -824,13 +824,13 @@ includeFile("events/elysiumTerminalMenuComponent_03.lua")
 includeFile("heroics/obi_wan_elysium.lua")
 
 -- Exar Kun
---includeFile("dungeon/exar_kun/exarKun.lua")
---includeFile("dungeon/exar_kun/exarKun2.lua")
---includeFile("dungeon/exar_kun/exarKun3.lua")
---includeFile("dungeon/exar_kun/exarKun4.lua")
+includeFile("dungeon/exar_kun/exarKun.lua")
+includeFile("dungeon/exar_kun/exarKun2.lua")
+includeFile("dungeon/exar_kun/exarKun3.lua")
+includeFile("dungeon/exar_kun/exarKun4.lua")
 
---includeFile("events/exarKunEntryMenuComponent.lua")
---includeFile("dungeon/exar_kun/exarKunEntryWay.lua")
+includeFile("events/exarKunEntryMenuComponent.lua")
+includeFile("dungeon/exar_kun/exarKunEntryWay.lua")
 
 -- Axkva Min
 includeFile("dungeon/axkva_min/axkvaMin.lua")
