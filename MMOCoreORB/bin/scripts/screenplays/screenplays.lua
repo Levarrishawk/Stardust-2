a5004134b6589803db268ea0392ede2a1269fbbf
Levarrishawk <levarrishawk@gmail.com>
2022-04-25 21:56:54 -0400

    [iteration] Activated Mustafar Screenplays

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -839,12 +839,15 @@ includeFile("events/axkvaMinEntryMenuComponent.lua")
 
 -- Mustafar
 includeFile("mustafar/boundaries/mustafar_boundaries.lua")
---[[
+
 includeFile("mustafar/mensix/mensix_mining_facility_main.lua")
+
+--[[
 includeFile("mustafar/quest/map_exploration.lua")
 includeFile("mustafar/quest/mining_field_markers.lua")
+--]]
+
 includeFile("mustafar/regions/mensix_facility_region.lua")
 includeFile("mustafar/regions/storm_lord_region.lua")
 includeFile("mustafar/regions/smoking_forest_region.lua")
 includeFile("mustafar/regions/north_west_region.lua")
---]]
