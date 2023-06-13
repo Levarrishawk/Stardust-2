d5efcd43b32c29b080cd1e660a19372f7349eec7
Levarrishawk <levarrishawk@gmail.com>
2023-06-12 23:30:48 -0400

    [iteration] Weapon Loot Schematic

---
+++ MMOCoreORB/bin/scripts/loot/items/loot_schematic/carbine_charric_schematic.lua
@@ -0,0 +1,12 @@
+carbine_charric_schematic = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/loot/loot_schematic/weapon_schematic/carbine_charric_schematic.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("carbine_charric_schematic", carbine_charric_schematic)
--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/weapon_schematic/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/weapon_schematic/objects.lua
@@ -1,8 +1,8 @@
 -- WEAPON SCHEMATICS
 -- -------------------------------------
-object_tangible_loot_loot_schematic_weapon_schematic_shared_charric_carbine_schematic = SharedTangibleObjectTemplate:new {
-  clientTemplateFileName = "object/tangible/loot/loot_schematic/weapon_schematic/shared_charric_carbine_schematic.iff"
+object_tangible_loot_loot_schematic_weapon_schematic_shared_carbine_charric_schematic = SharedTangibleObjectTemplate:new {
+  clientTemplateFileName = "object/tangible/loot/loot_schematic/weapon_schematic/shared_carbine_charric_schematic.iff"
   
 }
 
-ObjectTemplates:addClientTemplate(object_tangible_loot_loot_schematic_weapon_schematic_shared_charric_carbine_schematic, "object/tangible/loot/loot_schematic/weapon_schematic/shared_charric_carbine_schematic.iff")
+ObjectTemplates:addClientTemplate(object_tangible_loot_loot_schematic_weapon_schematic_shared_carbine_charric_schematic, "object/tangible/loot/loot_schematic/weapon_schematic/shared_carbine_charric_schematic.iff")
