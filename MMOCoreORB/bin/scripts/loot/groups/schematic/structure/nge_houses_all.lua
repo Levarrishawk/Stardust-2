07a79ec039c41aad1209d28f71b925025fc00559
Levarrishawk <levarrishawk@gmail.com>
2023-06-12 20:12:08 -0400

    [removed] Unused deeds

--- MMOCoreORB/bin/scripts/loot/groups/schematic/structure/nge_houses_all.lua
+++ MMOCoreORB/bin/scripts/loot/groups/schematic/structure/nge_houses_all.lua
@@ -13,11 +13,11 @@ nge_houses_all = {
     {itemTemplate = "generic_small_window_s02_loot_schematic", weight = 625000},
     {itemTemplate = "hangar_house_loot_schematic", weight = 625000},
     {itemTemplate = "jedi_house_loot_schematic", weight = 625000},
-    {itemTemplate = "naboo_small_window_s01_loot_schematic", weight = 625000},
+  --  {itemTemplate = "naboo_small_window_s01_loot_schematic", weight = 625000},
     {itemTemplate = "rebel_house_loot_schematic", weight = 625000},
     {itemTemplate = "relaxation_house_loot_schematic", weight = 625000},
     {itemTemplate = "sith_house_loot_schematic", weight = 625000},
-    {itemTemplate = "tatooine_small_window_s01_loot_schematic", weight = 625000},  
+   -- {itemTemplate = "tatooine_small_window_s01_loot_schematic", weight = 625000},  
     {itemTemplate = "musty_house_loot_schematic", weight = 625000},
     {itemTemplate = "vehicle_house_loot_schematic", weight = 625000}
   }
