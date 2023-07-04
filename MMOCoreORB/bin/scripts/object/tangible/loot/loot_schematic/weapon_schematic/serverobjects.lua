6b51b818197457c1678ffcd1154e62092039b697
Levarrishawk <levarrishawk@gmail.com>
2023-07-04 19:59:55 -0400

    [iteration] Weapon Loot Schematics
    TODO: insert into loot groups

--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/weapon_schematic/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/weapon_schematic/serverobjects.lua
@@ -8,71 +8,55 @@ includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_bothan_bola_s
 includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_charric_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_geo_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_kun_massassi_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_mandalorian_schematic.lua") --No Client IFF
 includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_proton_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_pvp_ngant_schematic.lua") --No Client IFF
---includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_pvp_schematic.lua") --No Client IFF
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_carbine_republic_sfor_schematic.lua")
-
--- Knucklers
 includeFile("tangible/loot/loot_schematic/weapon_schematic/melee_ep3_loot_soulstinger_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/melee_fan_metal_schematic.lua") -- No client IFF
---includeFile("tangible/loot/loot_schematic/weapon_schematic/melee_punch_dagger_schematic.lua") -- No client IFF
---includeFile("tangible/loot/loot_schematic/weapon_schematic/melee_pvp_bf_schematic.lua") -- No client IFF
-
---Pistols
 includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_heroic_sd_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_intimidator_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_mandalorian_schematic.lua") -- No client IFF
---includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_pvp_schematic.lua") -- No client iFF
 includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_renegade_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_westar_34_schematic.lua") -- No client iFF
-
---Polearms
 includeFile("tangible/loot/loot_schematic/weapon_schematic/polearm_lance_electric_polearm_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/polearm_lance_kaminoan_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/polearm_lance_kashyyk_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/polearm_lance_massassi_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/polearm_lance_shock_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/polearm_lance_staff_magna_guard_schematic.lua") --No client iff
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_lance_obsidian_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_lance_xandank_schematic.lua")
-
---Rifles
 includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_adventurer_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_bounty_dc15_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_geo_drill_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_ld1_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_mandalorian_schematic.lua") --No client iFF
 includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_massassi_ink_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_proton_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_tc22_blaster_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_westar_m5_schematic.lua") --No client iff
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_rifle_dp23_schematic.lua")
-
---Sword
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_sword_mustafar_bandit_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_sword_obsidian_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/sword_acid_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/sword_mace_junti_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/sword_mandalorian_schematic.lua") --No client iff
 includeFile("tangible/loot/loot_schematic/weapon_schematic/sword_marauder_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/sword_massassi_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/sword_rsf_schematic.lua")
-
---Two handed sword
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_two_handed_sword_obsidian_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/som_two_handed_sword_tulrus_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/two_handed_sword_kashyyk_schematic.lua")
 includeFile("tangible/loot/loot_schematic/weapon_schematic/two_handed_sword_kun_massassi_schematic.lua")
---includeFile("tangible/loot/loot_schematic/weapon_schematic/two_handed_sword_pvp_bf_01_schematic.lua") --No client iff
 includeFile("tangible/loot/loot_schematic/weapon_schematic/two_handed_sword_sith_schematic.lua")
 
 
-
-
-
-
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_pvp_ngant_schematic.lua") --No Client IFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_pvp_schematic.lua") --No Client IFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/carbine_mandalorian_schematic.lua") --No Client IFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/melee_fan_metal_schematic.lua") -- No client IFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/melee_punch_dagger_schematic.lua") -- No client IFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/melee_pvp_bf_schematic.lua") -- No client IFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_mandalorian_schematic.lua") -- No client IFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_pvp_schematic.lua") -- No client iFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/pistol_westar_34_schematic.lua") -- No client iFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/polearm_lance_staff_magna_guard_schematic.lua") --No client iff
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_mandalorian_schematic.lua") --No client iFF
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/rifle_westar_m5_schematic.lua") --No client iff
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/sword_mandalorian_schematic.lua") --No client iff
+--includeFile("tangible/loot/loot_schematic/weapon_schematic/two_handed_sword_pvp_bf_01_schematic.lua") --No client iff
 
 
 
