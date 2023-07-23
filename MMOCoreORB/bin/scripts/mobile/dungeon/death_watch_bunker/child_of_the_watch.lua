9186851f519cb8344ca7415b82c1d01bfc0bb9c5
Levarrishawk <levarrishawk@gmail.com>
2023-07-22 23:19:36 -0400

    [added] Mandalorian Covert
    Initial NPC Setup

---
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/child_of_the_watch.lua
@@ -0,0 +1,63 @@
+child_of_the_watch = Creature:new {
+	objectName = "@mob/creature_names:child_of_the_watch",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	mobType = MOB_NPC,
+	socialGroup = "death_watch",
+	faction = "",
+	level = 121,
+	chanceHit = 4,
+	damageMin = 745,
+	damageMax = 1200,
+	baseXp = 11390,
+	baseHAM = 50000,
+	baseHAMmax = 50000,
+	armor = 2,
+	resists = {55,55,70,60,30,30,100,40,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = NONE,
+	optionsBitmask = AIENABLED, INVULNERABLE,
+	diet = HERBIVORE,
+	scale = 1.05,
+
+	templates = {
+	   "object/mobile/dressed_death_watch_red.iff",
+	   "object/mobile/dressed_death_watch_silver.iff",
+	   "object/mobile/dressed_death_watch_grey.iff",
+	   "object/mobile/dressed_death_watch_gold.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "death_watch_bunker_commoners",   chance = 6300000},
+				{group = "death_watch_bunker_lieutenants", chance = 3500000},
+				{group = "death_watch_bunker_ingredient_protective",  chance = 100000},
+				{group = "death_watch_bunker_ingredient_binary",  chance = 100000}
+			},
+			lootChance = 1500000
+		}
+	},
+
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+	primaryWeapon = "pirate_weapons_heavy",
+	secondaryWeapon = "unarmed",
+	conversationTemplate = "",
+	reactionStf = "@npc_reaction/mandalorian",
+ -- personalityStf = "@hireling/hireling_stormtrooper",
+	
+	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+	primaryAttacks = merge(bountyhuntermaster,marksmanmaster,brawlermaster,tkamaster),
+	secondaryAttacks = { }
+}
+
+CreatureTemplates:addCreatureTemplate(child_of_the_watch, "child_of_the_watch")
