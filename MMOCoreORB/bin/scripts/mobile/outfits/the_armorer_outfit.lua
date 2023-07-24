5dfc532cb0ec8df6b7ab29b34847735609c9f4bc
Levarrishawk <levarrishawk@gmail.com>
2023-07-23 21:29:07 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/outfits/the_armorer_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/the_armorer_outfit.lua
@@ -2,13 +2,13 @@ the_armorer_outfit = {
 	{ 
 		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_bicep_l.iff", customizationVariables = {} },
 		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_bicep_r.iff", customizationVariables = {} },
-		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_boots.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/mandalorian_rebel/armor_mandalorian_rebel_boots.iff", customizationVariables = {} },
 		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_bracer_l.iff", customizationVariables = {} },
 		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_bracer_r.iff", customizationVariables = {} },
 		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_chest_plate.iff", customizationVariables = {} },
 		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_helmet.iff", customizationVariables = {} },
 		{objectTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_gloves.iff", customizationVariables = {} },
-		{objectTemplate = "object/tangible/wearables/armor/zam/armor_zam_wessel_pants.iff", customizationVariables = {} },	
+		{objectTemplate = "object/tangible/wearables/armor/zam/armor_zam_wesell_pants.iff", customizationVariables = {} },	
 	}
 	 
 }
