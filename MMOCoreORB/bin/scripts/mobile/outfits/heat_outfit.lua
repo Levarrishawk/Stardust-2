af2e8689ed57bbf0158a7ab27ba30cf30f3ebaf0
Levarrishawk <levarrishawk@gmail.com>
2023-08-07 00:20:14 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/outfits/heat_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/heat_outfit.lua
@@ -1,8 +1,8 @@
 heat_outfit = {
 	{ 
 		{objectTemplate = "object/tangible/wearables/helmet/helmet_fighter_imperial_ace.iff", customizationVariables = {} },
-		{objectTemplate = "object/tangible/wearables/pants/pants_s14.iff", customizationVariables = {{"/private/index_color_1", 42}} },
-		{objectTemplate = "object/tangible/wearables/jacket/jacket_s10.iff", customizationVariables = {{"/private/index_color_1", 42}} },
+		{objectTemplate = "object/tangible/wearables/pants/pants_s14.iff", customizationVariables = {{"/private/index_color_1", 87}} },
+		{objectTemplate = "object/tangible/wearables/jacket/jacket_s10.iff", customizationVariables = {{"/private/index_color_1", 87},{"/private/index_color_2", 87}} },
 		{objectTemplate = "object/tangible/wearables/boots/boots_s03.iff", customizationVariables = {} },
 	}
 	 
