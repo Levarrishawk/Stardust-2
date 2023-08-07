eb82b66c95e41a118cea55a873c7ca3f7516dc60
Levarrishawk <levarrishawk@gmail.com>
2023-08-07 00:36:15 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/screenplays/poi/naboo_memorial.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/naboo_memorial.lua
@@ -33,9 +33,10 @@ end
 function naboo_memorial:spawnSceneObjects()
 
  spawnSceneObject("naboo", "object/static/structure/naboo/garden_gazebo_lg_s01.iff", 5373, 7.5, 1525, 0, math.rad(0) )
- 
- 
- 
+ spawnSceneObject("naboo", "object/tangible/furniture/all/frn_all_tiki_torch_s1.iff", 5373.6, 7.7, 1527.2, 0, math.rad(0) )
+ spawnSceneObject("naboo", "object/tangible/furniture/all/frn_all_tiki_torch_s1.iff", 5375.2, 7.7, 1525.5, 0, math.rad(0) )
+ spawnSceneObject("naboo", "object/tangible/furniture/all/frn_all_tiki_torch_s1.iff", 5371.4, 7.7, 1526.6, 0, math.rad(0) )
+ spawnSceneObject("naboo", "object/tangible/furniture/all/frn_all_tiki_torch_s1.iff", 5370.8, 7.7, 1524.4, 0, math.rad(0) )
 
 end
 
@@ -47,4 +48,25 @@ function naboo_memorial:spawnMobiles()
 	
 	
 
+end
+
+function naboo_memorial:spawnActiveArea1()
+  local pSpawnArea1 = spawnSceneObject("lothal", "object/active_area.iff", 5373, 7, 1524, 0, 0)
+    
+  if (pSpawnArea1 ~= nil) then
+    local activeArea1 = LuaActiveArea(pSpawnArea1)
+          activeArea1:setCellObjectID(0)
+          activeArea1:setRadius(40)
+          createObserver(ENTEREDAREA, "naboo_memorial", "notifySpawnArea1", pSpawnArea1)          
+      end
+end
+
+function naboo_memorial:notifySpawnArea1(pActiveArea1, pMovingObject, pPlayer)
+  
+  if (not SceneObject(pMovingObject):isPlayerCreature()) then
+    return 0
+  else
+      CreatureObject(pMovingObject):playMusicMessage("sound/music_sadness.snd")
+  end          
+  return 0     
 end
