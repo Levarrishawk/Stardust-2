8ae0666311f2fabc815d0f887e67aea77baf9ac2
Levarris <levarrishawk@gmail.com>
2023-08-10 01:21:56 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bracer_l.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bracer_l.lua
@@ -112,7 +112,17 @@ object_tangible_wearables_armor_stormtrooper_armor_stormtrooper_bracer_l = objec
   heat = 60,
   cold = 60,
   acid = 60,
-  lightSaber = 0
+  lightSaber = 0,
+  
+  numberExperimentalProperties = {1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 1},
+  experimentalProperties = {"XX", "XX", "XX", "XX", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "XX", "XX", "XX", "XX", "OQ", "SR", "XX"},
+  experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  experimentalGroupTitles = {"null", "null", "null", "exp_durability", "exp_quality", "exp_resistance", "null", "null", "null", "null", "null", "exp_resistance", "null"},
+  experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
+  experimentalMin = {0, 0, 0, 1000, 1, 30000, 0, 0, 0, 1, 0, 0, 0},
+  experimentalMax = {0, 0, 0, 1000, 30, 50000, 0, 0, 0, 1, 0, 0, 0},
+  experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
+  experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_armor_stormtrooper_armor_stormtrooper_bracer_l, "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bracer_l.iff")
