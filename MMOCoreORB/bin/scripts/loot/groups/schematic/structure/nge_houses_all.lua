2eb2a7263f3ac5ac5239c73a87db2d991d747c04
Levarris <levarrishawk@gmail.com>
2023-08-11 14:16:58 -0400

    [hotfix] 2.0.1.6 - Fixed Looted House Schematics

--- MMOCoreORB/bin/scripts/loot/groups/schematic/structure/nge_houses_all.lua
+++ MMOCoreORB/bin/scripts/loot/groups/schematic/structure/nge_houses_all.lua
@@ -3,23 +3,26 @@ nge_houses_all = {
   minimumLevel = 0,
   maximumLevel = -1,
   lootItems = {
-    {itemTemplate = "bespin_house_loot_schematic", weight = 625000},
-    {itemTemplate = "emperors_house_loot_schematic", weight = 625000},
-    {itemTemplate = "generic_large_window_s01_loot_schematic", weight = 625000},
-    {itemTemplate = "generic_large_window_s02_loot_schematic", weight = 625000},
-    {itemTemplate = "generic_medium_window_s01_loot_schematic", weight = 625000},
-    {itemTemplate = "generic_medium_window_s02_loot_schematic", weight = 625000},
-    {itemTemplate = "generic_small_window_s01_loot_schematic", weight = 625000},
-    {itemTemplate = "generic_small_window_s02_loot_schematic", weight = 625000},
-    {itemTemplate = "hangar_house_loot_schematic", weight = 625000},
-    {itemTemplate = "jedi_house_loot_schematic", weight = 625000},
-  --  {itemTemplate = "naboo_small_window_s01_loot_schematic", weight = 625000},
-    {itemTemplate = "rebel_house_loot_schematic", weight = 625000},
-    {itemTemplate = "relaxation_house_loot_schematic", weight = 625000},
-    {itemTemplate = "sith_house_loot_schematic", weight = 625000},
-   -- {itemTemplate = "tatooine_small_window_s01_loot_schematic", weight = 625000},  
-    {itemTemplate = "musty_house_loot_schematic", weight = 625000},
-    {itemTemplate = "vehicle_house_loot_schematic", weight = 625000}
+
+    {itemTemplate = "emperors_house_loot_schematic", weight = 625000},  --ok
+    {itemTemplate = "generic_large_window_s01_loot_schematic", weight = 625000}, --ok
+    {itemTemplate = "generic_large_window_s02_loot_schematic", weight = 625000}, --ok
+    {itemTemplate = "generic_medium_window_s01_loot_schematic", weight = 625000}, --ok
+    {itemTemplate = "rebel_house_loot_schematic", weight = 625000},   -- ok
+    {itemTemplate = "sith_house_loot_schematic", weight = 625000}, -- ok   
+    {itemTemplate = "vehicle_house_loot_schematic", weight = 625000}, --ok  
+    {itemTemplate = "relaxation_house_loot_schematic", weight = 625000}, --ok
+    {itemTemplate = "jedi_house_loot_schematic", weight = 625000}, --ok
+    {itemTemplate = "generic_medium_window_s02_loot_schematic", weight = 625000}, --ok
+    {itemTemplate = "generic_small_window_s02_loot_schematic", weight = 625000}, --ok    
+    {itemTemplate = "generic_small_window_s01_loot_schematic", weight = 625000}
+                
+   -- {itemTemplate = "musty_house_loot_schematic", weight = 625000},  
+ -- {itemTemplate = "bespin_house_loot_schematic", weight = 625000}, --  ew 
+--  {itemTemplate = "hangar_house_loot_schematic", weight = 625000},
+--  {itemTemplate = "naboo_small_window_s01_loot_schematic", weight = 625000},
+ -- {itemTemplate = "tatooine_small_window_s01_loot_schematic", weight = 625000},  
+    
   }
 }
 
