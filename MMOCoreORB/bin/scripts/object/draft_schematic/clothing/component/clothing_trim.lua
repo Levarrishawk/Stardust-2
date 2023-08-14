7bf51cbbd6e6ba9490badb2b0272a5d0507e2b66
Levarrishawk <levarrishawk@gmail.com>
2023-08-14 01:20:43 -0400

    [hotfix] 2.0.1.7b - Adjusted Trim Factory Crate Size

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_trim.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_trim.lua
@@ -50,6 +50,7 @@ object_draft_schematic_clothing_component_clothing_trim = object_draft_schematic
    craftingToolTab = 8, -- (See DraftSchematicObjectTemplate.h)
    complexity = 1, 
    size = 1, 
+   factoryCrateSize = 1000,
 
    xpType = "crafting_clothing_general", 
    xp = 60, 
