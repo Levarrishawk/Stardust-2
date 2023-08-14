630b734c38900d8548075edb171e90d0dde183c4
Levarrishawk <levarrishawk@gmail.com>
2023-08-14 02:13:35 -0400

    [hotfix] 2.0.1.7c - Adjusted Difficulty of EK + AM instances

--- MMOCoreORB/bin/scripts/loot/groups/schematic/structure/nge_houses_all.lua
+++ MMOCoreORB/bin/scripts/loot/groups/schematic/structure/nge_houses_all.lua
@@ -9,10 +9,10 @@ nge_houses_all = {
     {itemTemplate = "generic_large_window_s02_loot_schematic", weight = 625000}, --ok
     {itemTemplate = "generic_medium_window_s01_loot_schematic", weight = 625000}, --ok
     {itemTemplate = "rebel_house_loot_schematic", weight = 625000},   -- ok
-    {itemTemplate = "sith_house_loot_schematic", weight = 625000}, -- ok   
+  --  {itemTemplate = "sith_house_loot_schematic", weight = 625000}, -- ok   
     {itemTemplate = "vehicle_house_loot_schematic", weight = 625000}, --ok  
     {itemTemplate = "relaxation_house_loot_schematic", weight = 625000}, --ok
-    {itemTemplate = "jedi_house_loot_schematic", weight = 625000}, --ok
+  --  {itemTemplate = "jedi_house_loot_schematic", weight = 625000}, --ok
     {itemTemplate = "generic_medium_window_s02_loot_schematic", weight = 625000}, --ok
     {itemTemplate = "generic_small_window_s02_loot_schematic", weight = 625000}, --ok    
     {itemTemplate = "musty_house_loot_schematic", weight = 625000} --ok
