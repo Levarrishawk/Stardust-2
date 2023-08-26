45df12b4fd3cfb8309d054bc812c69ec645cfb1f
Levarrishawk <levarrishawk@gmail.com>
2023-08-26 00:53:08 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/custom_content/mobile/exar_kun_cultist.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/mobile/exar_kun_cultist.lua
@@ -39,9 +39,16 @@ exar_kun_cultist = Creature:new {
 			lootChance = 4200000,
 		},			
 	},
-	weapons = {"mixed_force_weapons"},
-	conversationTemplate = "",
-	attacks = merge(fencermid,swordsmanmid,tkamid,pikemanmaster,brawlermaster)
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+  -- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+  primaryWeapon = "force_sword",
+  secondaryWeapon = "unarmed",
+  conversationTemplate = "",
+
+  -- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+  -- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+  primaryAttacks = merge(fencermaster,swordsmanmid,pikemanmaster,brawlermaster,forcewielder),
+  secondaryAttacks = forcewielder
 }
 
 CreatureTemplates:addCreatureTemplate(exar_kun_cultist, "exar_kun_cultist")
