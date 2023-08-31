4c2819dc9bc65a2a6fd39903a55e75d58f92e0ff
Levarrishawk <levarrishawk@gmail.com>
2023-08-31 01:04:43 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/managers/planet_manager.lua
+++ MMOCoreORB/bin/scripts/managers/planet_manager.lua
@@ -845,7 +845,7 @@ mustafar = {
   gcwEnabled = 0,
   
   planetTravelPoints = {
-    {name = "Mensix Mining Facility", x = -2471, z = 230, y = 1620, interplanetaryTravelAllowed = 1, incomingTravelAllowed = 0},
+    {name = "Mensix Mining Facility", x = -2471, z = 230, y = 1620, interplanetaryTravelAllowed = 0, incomingTravelAllowed = 0},
     },
     
     badgeAreas = {   
