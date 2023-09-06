f648098157a2094f9acb952193c3f943d00e3c6a
Levarris <levarrishawk@gmail.com>
2023-09-06 17:49:48 -0400

    [iteration] FA

--- MMOCoreORB/bin/scripts/commands/forceArmor1.lua
+++ MMOCoreORB/bin/scripts/commands/forceArmor1.lua
@@ -43,7 +43,7 @@
 
 ForceArmor1Command = {
     name = "forcearmor1",
-    forceCost = 35,
+    forceCost = 25,
     duration = 900,
     --animationCRC = hashCode()
     clientEffect = "clienteffect/pl_force_armor_self.cef",
