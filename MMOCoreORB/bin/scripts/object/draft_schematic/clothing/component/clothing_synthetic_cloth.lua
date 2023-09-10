d1be4e0f1240de459a7fe64a65c6776b339c23c2
Korralina <144608898+Korralina@users.noreply.github.com>
2023-09-10 10:58:11 -0400

    Update clothing_synthetic_cloth.lua
    Updated Synthetic Cloth crate to 1000

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_synthetic_cloth.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_synthetic_cloth.lua
@@ -49,7 +49,8 @@ object_draft_schematic_clothing_component_clothing_synthetic_cloth = object_draf
 
    craftingToolTab = 8, -- (See DraftSchematicObjectTemplate.h)
    complexity = 1, 
-   size = 2, 
+   size = 2,
+   factoryCrateSize = 1000,
 
    xpType = "crafting_clothing_general", 
    xp = 45, 
