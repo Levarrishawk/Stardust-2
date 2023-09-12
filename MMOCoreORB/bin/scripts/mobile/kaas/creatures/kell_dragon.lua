aef0208814f2db8a3c5a06f3230e6110c9c6d681
Levarris <levarrishawk@gmail.com>
2023-09-11 22:27:13 -0400

    [hotfix] 2.1.0.4 - Increased Loot Chances from Kell Dragons

--- MMOCoreORB/bin/scripts/mobile/kaas/creatures/kell_dragon.lua
+++ MMOCoreORB/bin/scripts/mobile/kaas/creatures/kell_dragon.lua
@@ -32,11 +32,11 @@ kell_dragon = Creature:new {
 	lootGroups = {
 		{
 	        groups = {
-				{group = "krayt_tissue_rare", chance = 2500000},
-        {group = "krayt_dragon_common", chance = 1500000},
+				{group = "krayt_tissue_rare", chance = 3500000},
+        {group = "krayt_dragon_common", chance = 2500000},
         {group = "krayt_pearls", chance = 1000000}			
 			},
-			lootChance = 5000000
+			lootChance = 7500000
 		},
 	},
 	primaryWeapon = "unarmed",
