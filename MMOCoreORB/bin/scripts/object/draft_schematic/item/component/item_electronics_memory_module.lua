120cb3f8d3499dddbbbc3c6bb5d74686d39eff27
Korralina <144608898+Korralina@users.noreply.github.com>
2023-09-12 11:32:00 -0400

    Update item_electronics_memory_module.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/item/component/item_electronics_memory_module.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/component/item_electronics_memory_module.lua
@@ -51,6 +51,7 @@ object_draft_schematic_item_component_item_electronics_memory_module = object_dr
    complexity = 12, 
    size = 2, 
    factoryCrateType = "object/factory/factory_crate_electronics.iff",
+   factoryCrateSize = 1000,
    
    xpType = "crafting_general", 
    xp = 40, 
