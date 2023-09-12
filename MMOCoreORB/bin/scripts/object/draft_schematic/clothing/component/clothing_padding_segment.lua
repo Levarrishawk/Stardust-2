b5b7c71d404e22f90bf1241023d19db65ae8febb
Korralina <144608898+Korralina@users.noreply.github.com>
2023-09-11 22:58:53 -0400

    Update clothing_padding_segment.lua
    padding segment factory crate size to 1k

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_padding_segment.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/component/clothing_padding_segment.lua
@@ -50,6 +50,7 @@ object_draft_schematic_clothing_component_clothing_padding_segment = object_draf
    craftingToolTab = 8, -- (See DraftSchematicObjectTemplate.h)
    complexity = 1, 
    size = 1, 
+   factoryCrateSize = 1000,
 
    xpType = "crafting_clothing_general", 
    xp = 55, 
