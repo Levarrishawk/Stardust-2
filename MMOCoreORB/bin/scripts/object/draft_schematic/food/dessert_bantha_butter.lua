ad923cdc36acd37cae1fcb492d65d5fe260acc49
Korralina <144608898+Korralina@users.noreply.github.com>
2023-09-11 23:18:56 -0400

    Update dessert_bantha_butter.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/food/dessert_bantha_butter.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/food/dessert_bantha_butter.lua
@@ -51,6 +51,7 @@ object_draft_schematic_food_dessert_bantha_butter = object_draft_schematic_food_
    complexity = 6, 
    size = 1, 
    factoryCrateType = "object/factory/factory_crate_food.iff",
+   factoryCrateSize = 1000,
    
    xpType = "crafting_food_general", 
    xp = 100, 
