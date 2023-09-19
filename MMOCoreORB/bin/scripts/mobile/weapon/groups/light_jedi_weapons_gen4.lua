8d6adc03b03fad6cc1c6a482bd491b477c319858
Levarrishawk <levarrishawk@gmail.com>
2023-09-18 20:44:57 -0400

    [iteration]

---
+++ MMOCoreORB/bin/scripts/mobile/weapon/groups/light_jedi_weapons_gen4.lua
@@ -0,0 +1,10 @@
+light_jedi_weapons_gen4 = {	
+	"object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_anakin.iff"
+}
+
+addWeapon("light_jedi_weapons_gen4", light_jedi_weapons_gen4)
+--To facilitate the correctness to Weapons groups
+--please include comments under here that point to your source
+--such as links to screenshots from wiki's that show pre-cu creature holding specific weapons
+--or links to cannon wiki pages showing a weapon belonging to a specific group in the Star Wars Universe
+--Source:
