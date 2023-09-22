088c34724985f0717c0d9dc1ebfb5513f54fb3ef
Levarrishawk <levarrishawk@gmail.com>
2023-09-21 23:16:57 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/screenplays/poi/elysium_vision.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/elysium_vision.lua
@@ -15,8 +15,15 @@ end
 
 function ElysiumVisionScreenPlay:spawnSceneObjects()
 
-  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -0.1, 12, 31.5, 590000047, math.rad(0) )
-
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -0.1, 12, 32.0, 590000047, math.rad(0) )
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -0.1, 16, 32.0, 590000047, math.rad(0) )
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -0.1, 20, 32.0, 590000047, math.rad(0) )
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -0.1, 24, 32.0, 590000047, math.rad(0) )
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -0.1, 28, 32.0, 590000047, math.rad(0) )
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -0.1, 30, 32.0, 590000047, math.rad(0) )
+  
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -14.9, 27, -147.9, 590000053, math.rad(90) )
+  spawnSceneObject("elysium", "object/static/structure/elysium/invisible_wall.iff", -3.9, 27, -172.7, 590000067, math.rad(90) )
 end
 
 function ElysiumVisionScreenPlay:spawnMobiles()
