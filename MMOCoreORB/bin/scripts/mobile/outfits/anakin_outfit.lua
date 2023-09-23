9fb29f47bc5e4f7b78d7e552004c9794c54693a2
Levarrishawk <levarrishawk@gmail.com>
2023-09-23 01:39:46 -0400

    [iteration

--- MMOCoreORB/bin/scripts/mobile/outfits/anakin_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/anakin_outfit.lua
@@ -1,7 +1,7 @@
 anakin_outfit = {
 	{ 
-		{objectTemplate = "object/tangible/hair/human/hair_human_male_s37.iff", customizationVariables = {{"/private/index_color_1", 46}}},
-		{objectTemplate = "object/tangible/wearables/robe/robe_jedi_dark_s04.iff", customizationVariables = {}},		
+		{objectTemplate = "object/tangible/hair/human/hair_human_male_s06.iff", customizationVariables = {{"/private/index_color_1", 46}}},
+		{objectTemplate = "object/tangible/wearables/robe/robe_jedi_dark_s03.iff", customizationVariables = {}},		
 		
 	}
 	 
