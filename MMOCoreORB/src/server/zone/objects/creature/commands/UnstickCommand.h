90063795c62d49e7519beb8fc09b564237dfbc04
Voxxy <70905380+Voxxy-Flurry@users.noreply.github.com>
2023-09-24 11:03:49 +0200

    Unstick adjustment.
    Added a cooldown to the Unstick command.

--- MMOCoreORB/src/server/zone/objects/creature/commands/UnstickCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/UnstickCommand.h
@@ -5,6 +5,8 @@
 #ifndef UNSTICKCOMMAND_H_
 #define UNSTICKCOMMAND_H_
 
+#include "server/zone/objects/scene/SceneObject.h"
+
 class UnstickCommand : public QueueCommand {
 public:
 
@@ -24,16 +26,25 @@ public:
 		if (creature != nullptr)
 			creature->error("used /unstick " + arguments.toString());
 
-		/*
-string/en/cmd_err.stf	7	unstick_in_progress	Unstick in progress
-string/en/cmd_err.stf	8	unstick_request_complete	Unstick complete
-string/en/cmd_err.stf	9	unstick_request_cancelled	Unstick request was cancelled
-		 */
+		CreatureObject* player = cast<CreatureObject*>(creature);
 
+		if (!player->checkCooldownRecovery("used_unstick")) {
+			player->sendSystemMessage("Please wait before using that command again.");
+			return 0;
+		} else {
+
+		player->addCooldown("used_unstick", 600000);
 		return SUCCESS;
+		}
+		
 	}
 
 };
 
 #endif //UNSTICKCOMMAND_H_
 
+		/*
+string/en/cmd_err.stf	7	unstick_in_progress	Unstick in progress
+string/en/cmd_err.stf	8	unstick_request_complete	Unstick complete
+string/en/cmd_err.stf	9	unstick_request_cancelled	Unstick request was cancelled
+		 */
