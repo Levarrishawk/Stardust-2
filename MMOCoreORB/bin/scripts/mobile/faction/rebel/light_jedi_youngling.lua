1c4d7496f07d9b093035fc7c550af4dbbdaa5fcf
Levarrishawk <levarrishawk@gmail.com>
2023-09-28 00:51:36 -0400

    [iteration]

---
+++ MMOCoreORB/bin/scripts/mobile/faction/rebel/light_jedi_youngling.lua
@@ -0,0 +1,53 @@
+light_jedi_youngling = Creature:new {
+	customName = "a Jedi Youngling",	
+	randomNameType = NAME_GENERIC,
+  randomNameTag = true,
+	socialGroup = "rebel",
+	faction = "rebel",
+	level = 30,
+	chanceHit = 23.5,
+	damageMin = 185,
+	damageMax = 280,
+	baseXp = 25266,
+	baseHAM = 1000,
+	baseHAMmax = 20000,
+	armor = 1,
+	-- {kinetic, energy, electric, stun, blast, heat, cold, acid, ls}
+	resists = {75,75,75,75,75,75,75,75,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 2 * 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+	lightsaberColor = 4,
+	scale = 0.5,
+
+	templates = {
+		"object/mobile/dressed_jedi_trainer_old_human_male_01.iff",
+		"object/mobile/dressed_jedi_trainer_chiss_male_01.iff",
+		"object/mobile/dressed_jedi_trainer_nikto_male_01.iff",
+		"object/mobile/dressed_jedi_trainer_twilek_female_01.iff",
+    	},
+	
+	lootGroups = {},
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+  -- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+  primaryWeapon = "light_jedi_weapons",
+  secondaryWeapon = "unarmed",
+  conversationTemplate = "",
+
+  -- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+  -- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+  primaryAttacks = lightsabermaster,
+  secondaryAttacks = forcepowermaster
+}
+
+CreatureTemplates:addCreatureTemplate(light_jedi_youngling, "light_jedi_youngling")
