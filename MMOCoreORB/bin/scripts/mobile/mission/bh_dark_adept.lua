25a6f56dc72fa120fa5177290ebce8a3512751e1
Levarrishawk <levarrishawk@gmail.com>
2023-10-14 19:55:06 -0400

    [changed] BH Armor Changes
    - Added Novice BH Certification requirement to equip BH Armor
    - Added schematic drops to top end BH NPC Targets

--- MMOCoreORB/bin/scripts/mobile/mission/bh_dark_adept.lua
+++ MMOCoreORB/bin/scripts/mobile/mission/bh_dark_adept.lua
@@ -32,11 +32,11 @@ bh_dark_adept = Creature:new {
 	lootGroups = {
 		{
 			groups = {
-				{group = "junk", chance = 2500000},
+				{group = "junk", chance = 3000000},
 				{group = "tailor_components", chance = 500000},
 				{group = "loot_kit_parts", chance = 500000},
 				{group = "color_crystals", chance = 500000},
-				{group = "power_crystals", chance = 1000000},
+				{group = "death_watch_bh_armor_schematics", chance = 500000}, 
 				{group = "wearables_all", chance = 1000000},
 				{group = "weapons_all", chance = 1000000},
 				{group = "armor_all", chance = 1000000},
@@ -47,11 +47,11 @@ bh_dark_adept = Creature:new {
 		},
 		{
 			groups = {
-				{group = "junk", chance = 2500000},
+				{group = "junk", chance = 3000000},
 				{group = "tailor_components", chance = 500000},
 				{group = "loot_kit_parts", chance = 500000},
 				{group = "color_crystals", chance = 500000},
-				{group = "power_crystals", chance = 1000000},
+				{group = "death_watch_bh_armor_schematics", chance = 500000}, 
 				{group = "wearables_all", chance = 1000000},
 				{group = "weapons_all", chance = 1000000},
 				{group = "armor_all", chance = 1000000},
@@ -62,12 +62,11 @@ bh_dark_adept = Creature:new {
 		},
 		{
 			groups = {
-				{group = "junk", chance = 2500000},
+				{group = "junk", chance = 3000000},
 				{group = "tailor_components", chance = 500000},
 				{group = "loot_kit_parts", chance = 500000},
 				{group = "color_crystals", chance = 500000},
-				{group = "power_crystals", chance = 500000},
-				{group = "power_crystals", chance = 500000},
+				{group = "death_watch_bh_armor_schematics", chance = 500000}, 				
 				{group = "wearables_all", chance = 1000000},
 				{group = "weapons_all", chance = 1000000},
 				{group = "armor_all", chance = 1000000},
