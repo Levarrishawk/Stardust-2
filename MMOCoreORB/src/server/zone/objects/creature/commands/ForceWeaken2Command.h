070d6b41e5c9e263f6afc035bfaff312560ab066
Voxxy <70905380+Voxxy-Flurry@users.noreply.github.com>
2023-10-28 15:41:51 +0200

    [Rework] Force Weaken 2
    Lowers the targets health & action by 30% (Scaling)
    Duration : 30s
    Hard Ability Cooldown : 60s
    Cannot be cast on multiple targets to bypass cooldown.
    Cannot be stacked with Force Weaken 1.

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceWeaken2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceWeaken2Command.h
@@ -7,9 +7,11 @@
 
 #include "server/zone/objects/scene/SceneObject.h"
 #include "ForcePowersQueueCommand.h"
-#include "server/zone/objects/creature/buffs/ForceWeakenDebuff.h"
 
 class ForceWeaken2Command : public ForcePowersQueueCommand {
+protected:
+	String skillName = "forceweaken2";
+	int delay = 60;
 public:
 
 	ForceWeaken2Command(const String& name, ZoneProcessServer* server)
@@ -35,29 +37,85 @@ public:
 			return INVALIDTARGET;
 		}
 
-		CreatureObject* creatureTarget = targetObject.castTo<CreatureObject*>();
-
-		if (creatureTarget->hasBuff(STRING_HASHCODE("forceweaken1")) || creatureTarget->hasBuff(STRING_HASHCODE("forceweaken2"))) {
-			return ALREADYAFFECTEDJEDIPOWER;
+		ManagedReference<CreatureObject*> creatureTarget = targetObject.castTo<CreatureObject*>();
+		
+		if (creatureTarget == nullptr){
+			return INVALIDTARGET;
 		}
+		
+		if (creatureTarget->hasBuff(BuffCRC::JEDI_FORCEWEAKEN1)) {
+			creature->sendSystemMessage("Target has already been weakened by a lesser force.");
+			return GENERALERROR;
+		}
+		
+		if (!creature->checkCooldownRecovery(skillName)){
+					const Time* timeRemaining = creature->getCooldownTime(skillName);
+					creature->sendSystemMessage("You must wait " + getCooldownString(timeRemaining->miliDifference() * -1) + " before attempting to weaken your target again.");
+					return GENERALERROR;
+					}
 
 		int res = doCombatAction(creature, target);
-
+		
+		
 		if (res == SUCCESS) {
-			Locker clocker(creatureTarget, creature);
 
-			ManagedReference<Buff*> buff = new ForceWeakenDebuff(creatureTarget, getNameCRC(), 400, 600, 120);
+				if (creatureTarget != nullptr) {
+				Locker clocker(creatureTarget, creature);
+				
+				ManagedReference<Buff*> buff = nullptr;
 
-			Locker locker(buff);
+				if (creature->isPlayerCreature())
+					buff = new Buff(creatureTarget, BuffCRC::JEDI_FORCEWEAKEN2, 30, BuffType::JEDI);
+				else
+					buff = new Buff(creatureTarget, BuffCRC::JEDI_FORCEWEAKEN2, 15, BuffType::JEDI);
 
-			creatureTarget->addBuff(buff);
 
-			CombatManager::instance()->broadcastCombatSpam(creature, creatureTarget, nullptr, 0, "cbt_spam", combatSpam + "_hit", 1);
+				if (buff == nullptr)
+					return GENERALERROR;
+								
+				Locker locker(buff);
+				int hamStrength =  -1 * creatureTarget->getMaxHAM(CreatureAttribute::HEALTH) * .3;
+				buff->setAttributeModifier(CreatureAttribute::HEALTH, hamStrength);
+				hamStrength =  -1 * creatureTarget->getMaxHAM(CreatureAttribute::ACTION) * .3;
+				buff->setAttributeModifier(CreatureAttribute::ACTION, hamStrength);
+
+				buff->setSkillModifier("weaken_delay", 5);
+
+				creatureTarget->addBuff(buff);
+				creature->updateCooldownTimer(skillName, delay * 1000);
+
+				CombatManager::instance()->broadcastCombatSpam(creature, creatureTarget, NULL, 0, "cbt_spam", combatSpam + "_hit", 1);
+				
+			}
+
 		}
 
 		return res;
 	}
 
+		String getCooldownString(uint32 delta) const {
+
+								int seconds = delta / 1000;
+
+								int hours = seconds / 3600;
+								seconds -= hours * 3600;
+
+								int minutes = seconds / 60;
+								seconds -= minutes * 60;
+
+								StringBuffer buffer;
+
+								if (hours > 0)
+									buffer << hours << "h ";
+
+								if (minutes > 0)
+									buffer << minutes << "m ";
+
+								if (seconds > 0)
+									buffer << seconds << "s";
+
+								return buffer.toString();
+							}
 };
 
 #endif //FORCEWEAKEN2COMMAND_H_
