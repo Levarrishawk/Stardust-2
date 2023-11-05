2c9340600300131f3d80e29315ff354f2cbe517e
mannax2012 <mannax@DESKTOP-MUVVHM7>
2023-11-05 14:35:30 -0600

    [FIXED] Unstable Gen3 - Now requires correct certification

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_unstable_gen3.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_unstable_gen3.lua
@@ -80,7 +80,7 @@ object_weapon_melee_2h_sword_crafted_saber_sword_lightsaber_two_handed_unstable_
   xpType = "jedi_general",
 
   -- See http://www.ocdsoft.com/files/certifications.xls
-  certificationsRequired = { "cert_twohandlightsaber_gen4" },
+  certificationsRequired = { "cert_twohandlightsaber_gen3" },
   -- See http://www.ocdsoft.com/files/accuracy.xls
   creatureAccuracyModifiers = { "twohandlightsaber_accuracy" },
 
